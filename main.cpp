5b35c712a5311311d73161c14f5f930897396f42
FlorianH123 <f.hennrich@gmx.de>
2017-11-22 12:02:57 +0100

    fktzfzkt

--- main.cpp
+++ main.cpp
@@ -17,35 +17,39 @@ int countWords(const char *string) {
 }
 
 int strcmp_ign_wsp(const char *s1, const char *s2) {
-    while (*s1 == *s2) {
-        while (*s1 == ' ') {
+    while (*s1 != '\0' && *s2 != '\0') {
+        if (*s1 == *s2) {
             *s1++;
-        }
-
-        while (*s2 == ' ') {
             *s2++;
+        } else if (*s1 == ' ') {
+            *s1++;
+        } else if (*s2 == ' ') {
+            *s2++;
+        } else {
+            if (*s1 > *s2) {
+                return 1;
+            } else if (*s1 < *s2) {
+                return -1;
+            }
         }
-
-        if (*s1 < *s2) {
-            return 1;
-        }
-
-        if (*s1 > *s2) {
-            return -1;
-        }
-
-        *s1++;
-        *s2++;
     }
 
-    return 0;
+    if (*s1 == '\0') {
+        if (*s2 == '\0') {
+            return 0;
+        } else {
+            return -1;
+        }
+    } else {
+        return 1;
+    }
 }
 
 int main() {
     char string[] = " Halldfsffff";
 
     //std::cout << countWords(" H a l \tl o ");
-    std::cout << strcmp_ign_wsp("   aa ab","a  aa");
+    std::cout << strcmp_ign_wsp("  aaa", " aaa");
 
     return 0;
 }
