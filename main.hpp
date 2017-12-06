98700f76c88b5d2c5e7842ec71f6a681a0bdb404
weedric <cedi.schreiner@web.de>
2017-12-06 11:16:57 +0100

    - Methodendefinitionen in header ausgelagert

--- main.hpp
+++ main.hpp
@@ -5,12 +5,14 @@
 #ifndef UEBUNG2_MAIN_HPP
 #define UEBUNG2_MAIN_HPP
 
+int countWords(const char *);
+int strcmp_ign_wsp(const char *, const char *);
+template <typename T>
+void insertionSort(T , T);
+template <typename T>
+void printArray(T, int);
 void printTestsPartA();
 void printTestsPartB();
 void printTestsPartC();
-template <typename T>
-void insertionSort(T first, T last);
-template <typename T>
-void printArray(T, int);
 
 #endif //UEBUNG2_MAIN_HPP
